4935366df7eafbe28ca53bd6aad8ec5b52992850
anamikachouragade <103032568+anamikachouragade@users.noreply.github.com>
2024-10-23 21:25:57 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -3,27 +3,32 @@ package codingTest;
 import java.util.Scanner;
 
 public class FibonacciSeries {
-	public static  void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-
-	    //input number of terms in the series
-	    System.out.print("Enter the number of terms: ");
-	
-	    int n= sc.nextInt();
-	
-	    int n1=0,n2=1;
+	public static void printFibonacci(int n) {
+            int n1=0,n2=1;
 	    
 	    System.out.println("Fibonacci series upto "+ n+ " terms:");
 	
 	     for (int i=1;i<=n;++i) {
 	    	 System.out.print(n1 + " ");
-
-		 //Calculating the next term
+	    	 
+	    	 //Calculate the next term
 	    	 int nextTerm= n1 +n2;
 	    	 n1=n2;
 	    	 n2= nextTerm;
 		
 	      }
+		
+	}
+	
+	public static  void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    
+	    //input number of terms
+	    System.out.print("Enter the number of terms: ");
+	
+	    int n= sc.nextInt();
+	
+	    printFibonacci(n);//calling the function to print the Fibonacci series
 	     
 	     sc.close();
 	
